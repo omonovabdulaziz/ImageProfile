f334ba695b9424a2c2f9a0b407a14229519a92df
omonovabdulaziz <omonov2006omonov@gmail.com>
2024-02-02 18:43:10 +0500

    tugadi

---
+++ main.py
@@ -0,0 +1,41 @@
+from telethon.sync import TelegramClient
+from telethon.tl import functions
+from datetime import datetime
+import time
+
+# Replace these values with your own API ID, API hash, and phone number
+api_id = '26311979'
+api_hash = '8f2d3c6a7f72f4210b1ede2e744ba3b8'
+phone_number = '+998950960153'
+
+# Create a new TelegramClient session
+client = TelegramClient('session_name', api_id, api_hash)
+
+
+async def update_name_status():
+    while True:
+        try:
+            current_time = datetime.now().strftime("%H:%M")
+
+            # Update your Telegram name with the current time
+            await client(functions.account.UpdateProfileRequest(
+                first_name=f'A. {current_time}'
+            ))
+
+            # Set your Telegram status to online
+            await client(functions.account.UpdateStatusRequest(
+                offline=False
+            ))
+
+            # Sleep for a short duration (adjust as needed)
+            time.sleep(60)
+        except Exception as e:
+            print(f"An error occurred: {e}")
+
+
+# Start the client
+client.start()
+
+# Run the update_name_status function in an event loop
+with client:
+    client.loop.run_until_complete(update_name_status())
